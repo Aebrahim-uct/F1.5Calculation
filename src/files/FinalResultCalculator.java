587e4857b96e759e80ae5668de9899a361c27b82
Abdullah <acee1@10.0.0.10>
2021-12-15 13:07:26 +0200

    Web Scraper Functional
    The web scraper is functional and can isolate the race results and
    remove the top 4 drivers.
    Need to:
    1) Clean up the code
    2) Look into WCC as well and calculations for it
    3) Do checks for fastest Lap point

--- src/files/FinalResultCalculator.java
+++ src/files/FinalResultCalculator.java
@@ -1,15 +1,19 @@
 package files;
 
-
 /**
  * Primary class that runs the entire calculation
+ * 
  * @author Abdullah
  *
  */
 public class FinalResultCalculator {
 
+	private static String[] races = { "Bahrain", "Emilia Romagna", "Portugal", "Spain", "Monaco", "Azerbaijan",
+			"France", "Styria", "Austria", "Great Britain", "Hungary", "Belgium", "Netherlands", "Italy", "Russia",
+			"Turkey", "United States", "Mexico", "Brazil", "Qatar", "Saudi Arabia", "Abu Dhabi" };
+
 	public static void main(String[] args) {
 		ResultCalculator res1 = new ResultCalculator("hello World");
-		
+
 	}
 }
--- src/files/webDataScraper.java
+++ src/files/webDataScraper.java
@@ -1,29 +1,80 @@
 package files;
 
+import java.time.Duration;
 //import java.io.FileWriter;
 //import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-/** 
- * 
+/**
+ * Scrapes the stats from the f1 website using Selenium. Removes the top two
+ * teams and their drivers.
  * 
  * @author acee1
  *
  */
 public class webDataScraper {
 
-	public webDataScraper() {
-		// TODO Auto-generated constructor stub
-		System.setProperty("webdriver.edge.driver", "C:\\Users\\acee1\\Downloads\\edgedriver_win64\\msedgedriver.exe");
+//	private String f1RaceURL = "https://www.formula1.com/en/results.html/2021/races.html";
+
+//	public webDataScraper() {
+	public static void main(String[] args) throws InterruptedException {
+
+		String inputRace = "Bahrain";
+		String f1RaceURL = "https://www.formula1.com/en/results.html/2021/races.html";
+		String[] toBeRemoved = { "Hamilton", "Verstappen", "Bottas", "Perez" };
+
+		System.setProperty("webdriver.edge.driver", "./edgedriver_win64/msedgedriver.exe");
+
+		WebDriver driver = new EdgeDriver();
+
+		driver.get(f1RaceURL);
+
+		WebElement cookieButton = driver.findElement(By.id("truste-consent-button"));
+		cookieButton.click();
+
+		List<WebElement> races = driver.findElements(By.xpath("//a[@class='dark bold ArchiveLink']"));
+
+		for (WebElement webEl : races) {
+			if (webEl.getText().equals(inputRace)) {
+				webEl.click();
+				break;
+			}
+		}
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions
+				.presenceOfAllElementsLocatedBy(By.xpath("//a[@class='side-nav-item-link ArchiveLink selected']")));
+
+		List<WebElement> raceResultWebEl = driver.findElements(By.xpath("//span[@class='hide-for-mobile']"));
+
+		
+		List<String> raceResult = new ArrayList<String>();
+		for (WebElement webEl : raceResultWebEl) {
+			raceResult.add(webEl.getAttribute("textContent"));
+		}
+		
+		for (String s : toBeRemoved) {
+			int pos=raceResult.indexOf(s);
+			raceResult.remove(pos);
+		}
+		for (String s : raceResult) {
+			System.out.println(s);
+		}
+//		Thread.sleep(10000);
+//		driver.close();
 
 	}
 
-	
-	
+//	public static void main(String[] args) {
+//		WebDriver driver = new EdgeDriver();
+//	}
+
 }
