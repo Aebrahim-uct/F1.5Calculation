0cdb09d5a3795a4614b57bbd0c6687cee875d580
Abdullah <acee163@gmail.com>
2022-04-11 17:50:58 +0200

    VSCode Port Changed IDE to VSCode and ported all code over. Minor changes to file hierarchy and testing pre-existing code. Lib files will be included in next Commit

---
+++ src/FinalResultCalculator.java
@@ -0,0 +1,13 @@
+/**
+ * Primary class that runs the entire calculation
+ * 
+ * @author Abdullah
+ *
+ */
+public class FinalResultCalculator {
+
+	public static void main(String[] args) {
+		// TODO Create WebScraper and return results from each race. Collate results and
+		// display within this class
+	}
+}
