2955f2e74ec00c436a90779291fec367c724448d
Abdullah <acee163@gmail.com>
2022-05-10 12:01:36 +0200

    Testing New Edits

--- src/my/f1_5/FinalResultCalculator.java
+++ src/my/f1_5/FinalResultCalculator.java
@@ -1,5 +1,9 @@
 package my.f1_5;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Primary class that runs the entire calculation
  * 
@@ -7,9 +11,20 @@ package my.f1_5;
  *
  */
 public class FinalResultCalculator {
+	private List<Map<String, Integer>> driverResults = new ArrayList<>();
+	private List<Map<String, Integer>> teamResults = new ArrayList<>();
+
+	private int[] racePoints = { 25, 18, 15, 12, 10, 8, 6, 4, 2, 1 };
+	private int[] sprintPoints = { 3, 2, 1 };
+	private int fastLap = 1;
 
 	public static void main(String[] args) {
 		// TODO Create WebScraper and return results from each race. Collate results and
 		// display within this class
+
+	}
+
+	private static void calcResults() {
+
 	}
 }
